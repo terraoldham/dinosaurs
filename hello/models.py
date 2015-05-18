47b500a0a3a9d6ef383ce41987341cfc8046417a
Terra Oldham <toldham@hearsaycorp.com>
2015-05-18 16:36:48 -0700

    Adding API interactions

--- hello/models.py
+++ hello/models.py
@@ -3,3 +3,13 @@ from django.db import models
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField('date created', auto_now_add=True)
+
+class InstagramPost(models.Model):
+    url = models.TextField(default=''),
+    tags = models.TextField(default=''),
+    caption = models.TextField(default=''),
+    username = models.TextField(default=''),
+    full_name = models.TextField(default=''),
+    likes = models.TextField(default=''),
+    creation_date = models.TextField(default=''),
+    location = models.TextField(default='')
--- hello/views.py
+++ hello/views.py
@@ -2,13 +2,29 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import requests
 from .models import Greeting
+import os
 
 # Create your views here.
 def index(request):
-    r = requests.get('http://httpbin.org/status/418')
-    print r.text
-    return HttpResponse('<pre>' + r.text + '</pre>')
-
+	r = requests.get('https://api.instagram.com/v1/media/popular?client_id=015f71721d534f73afeec647c844105b')
+	data = r.json()
+	items = data.get('data') # data['data']
+	item_id = 0 
+	
+	for i in items:
+		instagrampost = InstagramPost(
+			url = i['images']['standard_resolution']['url'],
+			tags = i['tags'],
+			caption = i['caption']['text'],
+			username = i['user']['username'],
+			full_name = i['user']['full_name'],
+			likes = i['likes']['like_count'],
+			creation_date = i['created_time'],
+			location = i['location']['name']	
+		)
+		instagrampost.save()
+		posts = instagrampost.objects.all()
+		return render(request, 'home.html', {'posts': [posts]})
 
 def db(request):
 
