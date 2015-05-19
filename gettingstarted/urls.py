b3b64ff47c6c6a3b93f63e8805c807ed8ce78a84
Terra Oldham <toldham@hearsaycorp.com>
2015-05-19 12:29:53 -0700

    Adding new page

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -12,6 +12,7 @@ urlpatterns = patterns('',
 
     url(r'^$', hello.views.index, name='index'),
     url(r'^db', hello.views.db, name='db'),
+    url(r'^dino/', hello.views.dino, name='dino'),
     url(r'^admin/', include(admin.site.urls)),
 
 )
--- hello/views.py
+++ hello/views.py
@@ -6,25 +6,33 @@ import os
 
 # Create your views here.
 def index(request):
-	r = requests.get('https://api.instagram.com/v1/media/popular?client_id=015f71721d534f73afeec647c844105b')
-	data = r.json()
-	items = data.get('data') # data['data']
+
+    if request.method == "POST":
+        return render(request, 'dino.html', {'dino_page_posts': [dino_page_posts]})
+		r = requests.get('https://api.instagram.com/v1/media/popular?client_id=015f71721d534f73afeec647c844105b')
+		data = r.json()
+		items = data.get('data') # data['data']
 	
-	for item in items:
-		caption = item['caption']['text'] if item['caption'] else None
-		creation_date = item['caption']['created_time'] if item['caption'] else None
-		instagrampost = InstagramPost.objects.create(
-			photo_url = item['images']['standard_resolution']['url'],
-			tag_text = item['tags'],
-			caption = caption,
-			creation_date = creation_date,
-			user_name = item['user']['username'],
-			fullname = item['user']['full_name'],
-			like_count = item['likes']['count'],	
-		)
+		for item in items:
+			caption = item['caption']['text'] if item['caption'] else None
+			creation_date = item['caption']['created_time'] if item['caption'] else None
+			instagrampost = InstagramPost.objects.create(
+				photo_url = item['images']['standard_resolution']['url'],
+				tag_text = item['tags'],
+				caption = caption,
+				creation_date = creation_date,
+				user_name = item['user']['username'],
+				fullname = item['user']['full_name'],
+				like_count = item['likes']['count'],	
+			)
 	    
-	posts = InstagramPost.objects.all()
-	return render(request, 'home.html', {'posts': posts})
+		posts = InstagramPost.objects.all()
+		#dino_page_posts = InstagramPosts.objects.get().order_by('-id')
+		
+		return render(request, 'dino.html', {'dino_page_posts', [dino_page_posts]})
+		
+	else: 
+		return render(request, 'home.html')
 
 def db(request):
 
@@ -34,4 +42,5 @@ def db(request):
     greetings = Greeting.objects.all()
 
     return render(request, 'db.html', {'greetings': greetings})
+    	
 
