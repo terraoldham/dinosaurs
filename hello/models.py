ddc6f841f83391dfbf4a681befcdb383d1b03c4f
Terra Oldham <toldham@hearsaycorp.com>
2015-05-18 17:23:38 -0700

    Updating Endpoints

--- hello/models.py
+++ hello/models.py
@@ -5,10 +5,10 @@ class Greeting(models.Model):
     when = models.DateTimeField('date created', auto_now_add=True)
 
 class InstagramPost(models.Model):
-    photo_url = models.TextField(default=''),
-    tag_text = models.TextField(default=''),
-    caption = models.TextField(default=''),
-    user_name = models.TextField(default=''),
-    fullname = models.TextField(default=''),
-    like_count = models.TextField(default=''),
+    photo_url = models.TextField(default='')
+    tag_text = models.TextField(default='')
+    caption = models.TextField(default='')
+    user_name = models.TextField(default='')
+    fullname = models.TextField(default='')
+    like_count = models.TextField(default='')
     creation_date = models.TextField(default='')
