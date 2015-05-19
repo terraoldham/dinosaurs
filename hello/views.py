4f09f407f54c5c4ac33b266a989ce1caddf9e794
Terra Oldham <toldham@hearsaycorp.com>
2015-05-18 17:40:38 -0700

    Updating Endpoints

--- hello/views.py
+++ hello/views.py
@@ -9,17 +9,17 @@ def index(request):
 	r = requests.get('https://api.instagram.com/v1/media/popular?client_id=015f71721d534f73afeec647c844105b')
 	data = r.json()
 	items = data.get('data') # data['data']
-	item_id = 0 
 	
-	for i in items:
+	for item in items:
+		caption = item['caption']['text'] if item['caption'] else None
 		instagrampost = InstagramPost.objects.create(
-			photo_url = i['images']['standard_resolution']['url'],
-			tag_text = i['tags'],
-			caption = i['caption']['text'],
-			user_name = i['user']['username'],
-			fullname = i['user']['full_name'],
-			like_count = i['likes']['count'],
-			creation_date = i['caption']['created_time'],	
+			photo_url = item['images']['standard_resolution']['url'],
+			tag_text = item['tags'],
+			caption = caption,
+			user_name = item['user']['username'],
+			fullname = item['user']['full_name'],
+			like_count = item['likes']['count'],
+			creation_date = item['caption']['created_time'],	
 		)
 	    
 	posts = InstagramPost.objects.all()
