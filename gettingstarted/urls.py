9ac8dd8d3596833955cd88f8487223384964319e
Terra Oldham <toldham@hearsaycorp.com>
2015-05-19 12:41:06 -0700

    Adding new page

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -12,7 +12,7 @@ urlpatterns = patterns('',
 
     url(r'^$', hello.views.index, name='index'),
     url(r'^db', hello.views.db, name='db'),
-    url(r'^dino/', hello.views.dino, name='dino'),
+    url(r'^dino/', hello.views.index, name='index'),
     url(r'^admin/', include(admin.site.urls)),
 
 )
