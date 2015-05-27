df0c5edd9fd74813ee4ab31393256b1c7c72e07c
Terra Oldham <toldham@hearsaycorp.com>
2015-05-27 16:40:47 -0700

    add css

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -11,9 +11,9 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^$', hello.views.index, name='index'),
-    url(r'^db', hello.views.db, name='db'),
-    url(r'^dino/', hello.views.index, name='dino'),
-    url(r'^all/', hello.views.index, name='all'),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^db$', hello.views.db, name='db'),
+    url(r'^dino/$', hello.views.index, name='dino'),
+    url(r'^all/$', hello.views.index, name='all'),
+    url(r'^admin/$', include(admin.site.urls)),
 
 )
