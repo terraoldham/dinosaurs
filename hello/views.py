286c50ce96b8660837309f25f54c82d1a045aa0e
Terra Oldham <toldham@hearsaycorp.com>
2015-05-27 17:25:58 -0700

    display for GET

--- hello/views.py
+++ hello/views.py
@@ -7,24 +7,24 @@ import os
 # Create your views here.
 def index(request):
 
-#if request.method == "POST":
-	r = requests.get('https://api.instagram.com/v1/media/popular?client_id=015f71721d534f73afeec647c844105b')
-	data = r.json()
-	items = data.get('data') 
+	if request.method == "POST":
+		r = requests.get('https://api.instagram.com/v1/media/popular?client_id=015f71721d534f73afeec647c844105b')
+		data = r.json()
+		items = data.get('data') 
 	
-	for item in items:
-		caption = item['caption']['text'] if item['caption'] else None
-		creation_date = item['caption']['created_time'] if item['caption'] else None
-		like_count = item['likes']['count'] if item['likes'] else None
-		instagrampost = InstagramPost.objects.create(
-			photo_url = item['images']['standard_resolution']['url'],
-			tag_text = item['tags'],
-			caption = caption,
-			creation_date = creation_date,
-			user_name = item['user']['username'],
-			fullname = item['user']['full_name'],
-			like_count = like_count	
-		)
+		for item in items:
+			caption = item['caption']['text'] if item['caption'] else None
+			creation_date = item['caption']['created_time'] if item['caption'] else None
+			like_count = item['likes']['count'] if item['likes'] else None
+			instagrampost = InstagramPost.objects.create(
+				photo_url = item['images']['standard_resolution']['url'],
+				tag_text = item['tags'],
+				caption = caption,
+				creation_date = creation_date,
+				user_name = item['user']['username'],
+				fullname = item['user']['full_name'],
+				like_count = like_count	
+			)
 	    
 	dino_page_posts = InstagramPost.objects.order_by('-id') 
 	return render(request, 'dino.html', {'dino_page_posts': dino_page_posts}) #returns all of the dinoposts 
