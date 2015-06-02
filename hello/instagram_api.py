d3c7245ca2f95c0d31fff7facd1054c30809ec63
Terra Oldham <toldham@hearsaycorp.com>
2015-06-02 16:17:10 -0700

    remove 'all' url

--- hello/instagram_api.py
+++ hello/instagram_api.py
@@ -3,14 +3,14 @@ import requests
 from .models import Greeting, InstagramPost
 import os
 
-def popular_posts(request):
+def popular_posts():
 	if request.method == "POST":
 		r = requests.get('https://api.instagram.com/v1/media/popular?client_id=015f71721d534f73afeec647c844105b')
 		data = r.json()
 		items = data.get('data')
 	return items
 
-def get_items(request):
+def get_items():
 	caption = item['caption']['text'] if item['caption'] else None
 	creation_date = item['caption']['created_time'] if item['caption'] else None
 	like_count = item['likes']['count'] if item['likes'] else None
